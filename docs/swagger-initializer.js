5b3db4460c5284212bcbd558820e310880484698
nandantal <nandan.dutta@talentica.com>
2025-08-02 13:35:08 +0530

    api

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "./swagger/complete-api-reference.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
@@ -14,6 +14,19 @@ window.onload = function() {
       SwaggerUIBundle.plugins.DownloadUrl
     ],
     layout: "StandaloneLayout"
+  });
+
+  // OAuth configuration
+  window.ui.initOAuth({
+    clientId: "your-client-id",
+    clientSecret: "your-client-secret-if-required",
+    realm: "your-realms",
+    appName: "your-app-name",
+    scopeSeparator: " ",
+    scopes: "openid profile email phone address",
+    additionalQueryStringParams: {},
+    useBasicAuthenticationWithAccessCodeGrant: false,
+    usePkceWithAuthorizationCodeGrant: false
   });
 
   //</editor-fold>
